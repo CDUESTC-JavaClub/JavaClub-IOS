561ff875b1741ddf745ad669bd0b09b55deb8c51
Roy <royrao2333@gmail.com>
2021-09-26 18:16:20 +0800

    Start Settings View

--- JavaClub/JavaClub/Settings/View/STMainViewController.swift
+++ JavaClub/JavaClub/Settings/View/STMainViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import SnapKit
 
 class STMainViewController: UIViewController {
-
+    private let contentVC: STContentViewController!
+    
+    init() {
+        contentVC = UIStoryboard(name: "Settings", bundle: .main)
+            .instantiateViewController(withIdentifier: "STContentViewController")
+        as? STContentViewController
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        view.addSubview(contentVC.view)
+        contentVC.view.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
     }
 }
